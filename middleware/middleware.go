ba4e3bb2c759ab5f30f8fb340073fcac14d6427e
Rahul Manthena <rahulmanthena@gmail.com>
2016-12-27 23:49:20 -0500

    --

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"os"
 
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+
 	"github.com/gorilla/sessions"
 )
 
@@ -12,10 +15,13 @@ var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET_KEY")))
 // AuthMiddleware is run before all the handler to make sure user is logged in
 func AuthMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		session, _ := store.Get(r, "session")
-		if session.IsNew || session.Values["user"] == nil {
+		ctx := appengine.NewContext(r)
+		session, err := store.Get(r, "session")
+		if session.IsNew || session.Values["user"] == nil || err != nil {
 			//not logged in, show login page
+			log.Errorf(ctx, "AuthMiddleware : Error in retrieving session", err, session)
 			http.Redirect(w, r, "/", 302)
+			return
 		}
 		h.ServeHTTP(w, r)
 	})
