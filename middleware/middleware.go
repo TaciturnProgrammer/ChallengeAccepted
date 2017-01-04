0d862958a20c68ca1f38feb589319f73b7d83b45
Rahul Manthena <rahulmanthena@gmail.com>
2017-01-03 22:36:42 -0500

    Added Panic recovery handler

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -25,5 +25,19 @@ func AuthMiddleware(h http.Handler) http.Handler {
 		}
 		h.ServeHTTP(w, r)
 	})
+}
 
+// RecoverHandler is run before all the handlers to handle panics gracefully
+func RecoverHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			ctx := appengine.NewContext(r)
+			if err := recover(); err != nil {
+				log.Criticalf(ctx, "panic: %+v", err)
+				http.Error(w, http.StatusText(500), 500)
+			}
+		}()
+
+		h.ServeHTTP(w, r)
+	})
 }
